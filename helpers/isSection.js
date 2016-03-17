e184d2ef334709857787c92063c3e0d93ee5442e
Ben Chidgey <ben.chidgey@digital.hmrc.gov.uk>
2016-03-17 10:03:26 +0000

    added comment explaining isSection helper

--- helpers/isSection.js
+++ helpers/isSection.js
@@ -3,6 +3,17 @@
 module.exports.register = function(handlebars, config) {
   config = config || {};
 
+  /**
+   * This helper can be used as a conditional to determine if you are in a particular section, it will detect
+   * dependant on the sections rootName
+   *
+   * usage:
+   * {{#isSection "AutoComplete"}}
+   *  is AutoComplete
+   * {{else}}
+   *  is NOT AutoComplete
+   * {{/isSection}}
+   */
   handlebars.registerHelper('isSection', function(rootName, options) {
     if (options.data.root.rootName.toLowerCase() === rootName.toLowerCase()) {
       return options.fn(this);
