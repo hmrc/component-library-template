196938043ab162e0feb78f060c35f9805da65330
Ben Chidgey <ben.chidgey@digital.hmrc.gov.uk>
2016-04-01 16:37:36 +0100

    updates to isSection helper to normalize input

--- helpers/isSection.js
+++ helpers/isSection.js
@@ -5,7 +5,8 @@ module.exports.register = function(handlebars, config) {
 
   /**
    * This helper can be used as a conditional to determine if you are in a particular section, it will detect
-   * dependant on the sections rootName
+   * dependant on the sections rootName.
+   * NOTE: It will normalise the submitted rootName and the stored rootName to a lowercase version without spaces.
    *
    * usage:
    * {{#isSection "AutoComplete"}}
@@ -15,7 +16,13 @@ module.exports.register = function(handlebars, config) {
    * {{/isSection}}
    */
   handlebars.registerHelper('isSection', function(rootName, options) {
-    if (options.data.root.rootName.toLowerCase() === rootName.toLowerCase()) {
+    var normalise = function (string) {
+      return string.replace(/\s/g, '').toLowerCase();
+    }
+    var sectionRootName = normalise(options.data.root.rootName);
+    var rootName = normalise(rootName);
+
+    if (sectionRootName === rootName) {
       return options.fn(this);
     } else {
       return options.inverse(this);
