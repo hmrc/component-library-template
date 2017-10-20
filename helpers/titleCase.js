703e94e0790af3b64be203bce4fce657ae74e9fe
csabascript <balint.csaba.imre@gmail.com>
2017-10-20 12:01:15 +0100

    fix regex for titleCase split

--- helpers/titleCase.js
+++ helpers/titleCase.js
@@ -1,6 +1,6 @@
 module.exports = function titleCase (text) {
   return text
-    .split(/[ -_]/)
+    .split(/[ \-_]/)
     .map(function (word) {
       return word.charAt(0).toUpperCase() + word.substr(1).toLowerCase()
     })
